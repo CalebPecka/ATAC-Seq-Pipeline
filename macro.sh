e4120aedbca89bfbefde5e7f1f6754cd8721d0c5
CalebPecka <57808677+CalebPecka@users.noreply.github.com>
2020-08-14 09:44:48 -0500

    Update macro.sh

--- macro.sh
+++ macro.sh
@@ -1,2 +1,3 @@
 macs2 callpeak -t $1 -g hs -f BAM -p 0.05 --seed 0 --bdg --outdir $2
 bamCoverage -b $1 -o $2/bigWig_coverage.bw
+make_tracks_file --trackFiles $2/bigWig_coverage.bw $2/NA_peaks.narrowPeak genes.bed -o $2/tracks.ini
