2c274788d3036da96badffac76a3b79d338ad4c6
CalebPecka <57808677+CalebPecka@users.noreply.github.com>
2021-05-18 16:09:23 -0500

    Delete macro.sh

--- macro.sh
+++
@@ -1,4 +0,0 @@
-macs2 callpeak -t $1 -g hs -f BAM -p 0.05 --seed 0 --bdg --outdir $2
-bamCoverage -b $1 -o $2/bigWig_coverage.bw
-make_tracks_file --trackFiles $2/bigWig_coverage.bw $2/NA_peaks.narrowPeak genes.bed -o $2/tracks.ini
-Rscript NarrowPeakSummitTracker.R $2/NA_peaks.narrowPeak GFFgenes.bed $2
